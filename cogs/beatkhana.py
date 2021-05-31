0f6b104040caf73de9aca4371e156d055dbffa98
Sirspam <71392316+Sirspam@users.noreply.github.com>
2021-05-31 16:29:51 +0100

    Added tourney, maps and bracket functions

--- cogs/beatkhana.py
+++ cogs/beatkhana.py
@@ -5,7 +5,6 @@ import logging
 import json
 from discord.ext import commands
 from json.decoder import JSONDecodeError
-from utils.role_checks import guild_coord_role_check
 
 
 class BeatKhana(commands.Cog):
@@ -13,8 +12,7 @@ class BeatKhana(commands.Cog):
         self.bot = bot
 
 
-    @commands.command(help="Gets information on a user from BeatKhana!", aliases=["bk"])
-    @guild_coord_role_check()
+    @commands.group(invoke_without_command=True, help="Gets information on a user from BeatKhana!", aliases=["bk"])
     async def beatkhana(self, ctx, user:discord.User):
         logging.info(f"beatkhana invoked with {user} in {ctx.guild.name}")
         async with ctx.channel.typing():
@@ -54,6 +52,59 @@ class BeatKhana(commands.Cog):
             )
         await ctx.reply(embed=embed)
         logging.info("beatkhana concluded")
+
+    @beatkhana.command(help="Gets general information on the tournament", aliases=["tourney","t"])
+    async def tournament(self, ctx):
+        logging.info(f"beatkhana tournament invoked in {ctx.guild.name}")
+        async with ctx.channel.typing():
+            async with self.bot.session.get(f"https://beatkhana.com/api/tournament/{self.bot.config[str(ctx.guild.id)]['beatkhana_id']}") as resp:
+                json_data = json.loads(await resp.text())[0]
+            embed = discord.Embed(
+                title=json_data["name"],
+                description=json_data["twitchLink"],
+                colour=0xc8825a,
+                url=f"https://beatkhana.com/tournament/{self.bot.config[str(ctx.guild.id)]['beatkhana_id']}"
+            )
+            embed.set_thumbnail(url=f"https://beatkhana.com/assets/images/{json_data['image']}")
+            await ctx.send(embed=embed)
+
+    @beatkhana.command(help="Gets information on the tournament map pool", aliases=["map","m"])
+    async def maps(self, ctx):
+        logging.info(f"beatkhana maps invoked in {ctx.guild.name}")
+        async with ctx.channel.typing():
+            async with self.bot.session.get(f"https://beatkhana.com/api/tournament/{self.bot.config[str(ctx.guild.id)]['beatkhana_id']}") as resp:
+                tourney_json_data = json.loads(await resp.text())[0]
+            async with self.bot.session.get(f"https://beatkhana.com/api/tournament/{self.bot.config[str(ctx.guild.id)]['beatkhana_id']}/map-pools") as resp:
+                pool_json_data = json.loads(await resp.text())
+        embed=discord.Embed(colour=0xc8825a)
+        embed.set_author(
+            name=f"{tourney_json_data['name']} Map Pools",
+            url=f"https://beatkhana.com/tournament/{self.bot.config[str(ctx.guild.id)]['beatkhana_id']}/map-pool",
+            icon_url=f"https://beatkhana.com/assets/images/{tourney_json_data['image']}"
+        )
+        for pool in pool_json_data:
+            message = str()
+            for song in pool_json_data[pool]["songs"]:
+                message = f"{message}[{song['name']} - {song['songAuthor']}](https://beatsaver.com/beatmap/{song['key']}) [{song['levelAuthor']}] - **{song['diff']}** ``{song['key']}``\n"
+            message = f"{message}\n**[[Download Pool]](https://beatkhana.com/api/download-pool/{pool})**"
+            embed.add_field(
+                name=pool_json_data[pool]["poolName"],
+                value=message,
+                inline=False
+            )
+        try:
+            await ctx.send(embed=embed)
+        except discord.HTTPException:
+            await ctx.send(embed=discord.Embed(
+            title="Uh oh. I couldn't send the map pool embed <:NotLikeAqua:822089498866221076>",
+            description=f"**[Here's a link to BeatKhana's map pool page](https://beatkhana.com/tournament/{self.bot.config[str(ctx.guild.id)]['beatkhana_id']}/map-pool)**\n\nThis was likely caused by the embed going over discord's length cap.\nYou can easily fix this by removing some maps from your map pool <:AquaTroll:845802819634462780>",
+            colour=discord.Colour.red()
+        ))
+        logging.info("Beatkhana maps concluded")
+
+    @beatkhana.command(help="Gets information on the tournament bracket", aliases=["bracket","b"])
+    async def brackets(self, ctx):
+        logging.info(f"beatkhana brackets invoked in {ctx.guild.name}")
 
 
 def setup(bot):
