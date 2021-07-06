3509d2e201ab783859f86d7a29ed1e3de40255dd
Sirspam <Charlespattison555@Gmail.com>
2021-07-06 09:09:54 +0100

    Removed hard coded invite link

--- cogs/general.py
+++ cogs/general.py
@@ -1,11 +1,12 @@
 import logging
-
-from discord import Embed, File
 from io import StringIO
 from datetime import datetime
 
+from discord import Embed, File, Permissions
+
 from discord.errors import NotFound
 from discord.ext import commands
+from discord.utils import oauth_url
 from utils.role_checks import admin_or_bot_owner_check
 
 
@@ -14,13 +15,32 @@ class General(commands.Cog):
         self.bot = bot
 
 
-    @commands.command(aliases=["link"], help="Links relevant for the bot")
+    @commands.command(aliases=["invite"])
     async def links(self, ctx):
+        permission_names = (
+            "read_messages",
+            "send_messages",
+            "manage_messages",
+            "embed_links",
+            "attach_files",
+            "read_message_history",
+            "add_reactions",
+            "use_external_emojis"
+            "mute_members",
+            "deafen_members",
+            "move_members"
+        )
+        perms = Permissions()
+        perms.update(**dict.fromkeys(permission_names, True))
         embed = Embed(
-            description="[Bot Invite Link](https://discord.com/api/oauth2/authorize?client_id=813699805150838795&permissions=29748288&scope=bot)\n[Home Server](https://discord.gg/dWX6fpGUK9)\n[Github Repo](https://github.com/Sirspam/Coordy-McCoordFace)\n\nI hope you're having a good day :)",
+            description=
+f"""[**Bot Invite Link**]({oauth_url(self.bot.user.id, perms)})
+[**Home Server**](https://discord.gg/dWX6fpGUK9)
+[**Github Repo**](https://github.com/Sirspam/Coordy-McCoordFace)\n
+I hope you're having a good day :)""",
             color=0x00A9E0)
         embed.set_thumbnail(url="https://cdn.discordapp.com/emojis/787809230639202354.png?v=1")
-        await ctx.send(embed=embed)
+        await ctx.reply(embed=embed)
 
     @commands.command(aliases=["nick"], help="Changes the bot's nickname")
     @admin_or_bot_owner_check()
